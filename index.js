8bfff5efec860ca919895bd787642dff3a789a5d
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-10 05:03:19 +0800

    Challenge 1 complete, Challenge 2 in progress

---
+++ index.js
@@ -0,0 +1,45 @@
+const menu = [
+    {
+        name: "Cobra",
+        price: 10
+    },
+    {
+        name: "Sting",
+        price: 15
+    },
+]
+
+let cashInRegister = 100
+const orderQueue = []
+
+function addNewPizza(pizzaObject) {
+    menu.push(pizzaObject)
+}
+
+/**
+ * Write another utility function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ */
+
+function placeOrder(pizzeName) {
+    const pizza = menu.filter(obj => obj.name === pizzeName)
+
+    if (pizza.length > 0) {
+        const oldCash = cashInRegister
+        const price = pizza.price
+        
+        cashInRegister = cashInRegister + parseInt(price)
+
+        console.log("New Cash from", oldCash, "into", parseInt(cashInRegister))
+
+        console.log(pizza)
+    } else {
+        console.log("Pizza name not found")
+    }
+}
+
+placeOrder("Sting")
