39d39561f8ea95c22f10325798c848a630b6a5e9
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-12 20:17:30 +0800

    solved problem, but processing another entry bugs

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const menu = [
 ]
 
 let cashInRegister = 100
-const orderQueue = []
+let orderQueue = []
+let nextOrderId = 1;
 
 function addNewPizza(pizzaObject) {
     menu.push(pizzaObject)
@@ -20,7 +21,8 @@ function addNewPizza(pizzaObject) {
 function placeOrder(pizzaName) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
     cashInRegister += selectedPizza.price
-    const newOrder = { pizza: selectedPizza, status: "ordered" }
+    const newOrder = { pizza: selectedPizza, status: "ordered", orderId: nextOrderId}
+    nextOrderId += 1
     orderQueue.push(newOrder)
     return newOrder
 }
@@ -32,3 +34,21 @@ function placeOrder(pizzaName) {
  * 
  * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database.
  */
+
+function completeOrder( orderId) {
+    const order = orderQueue.find(orderObj => orderObj.orderId)
+
+    if (order) {
+        orderQueue = orderQueue.filter(obj => obj.orderId !== orderId)
+        order["status"] = 'completed'
+        orderQueue.push(order)
+        return order
+    } else {
+        return "Order not found"
+    }
+}
+
+console.log(placeOrder("Sting"))
+console.log(completeOrder(1))
+console.log(placeOrder("Cobra"))
+console.log(completeOrder(2))
