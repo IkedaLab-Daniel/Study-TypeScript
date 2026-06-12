69a68675190ef735e963c1619ff9aa2aa3ad8207
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-12 20:22:16 +0800

    add test process (with bugs, to showcase how ts will solve such issues)

--- index.js
+++ index.js
@@ -31,3 +31,14 @@ function completedOrder(orderId) {
     order.status = "completed"
     return order
 }
+
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+addNewPizza({ name: "BBQ Chicken", cost: 12 })
+addNewPizza({ name: "Spicy Sausage", cost: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completedOrder("1")
+
+console.log("Menu", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
