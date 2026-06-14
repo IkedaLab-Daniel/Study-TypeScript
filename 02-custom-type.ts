e68e3f5e431c896a912080ca6ea3fb8e3f32d7a1
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-14 21:11:23 +0800

    add address type

--- 02-custom-type.ts
+++ 02-custom-type.ts
@@ -1,23 +1,33 @@
-type Food = string
-let favoriteFood: Food = "pizza"
-
-// > ---------
-
 type Person = {
     name: string
     age: number
     isStudent: boolean
+    address: {
+        street: string
+        city: string
+        country: string
+    }
 }
 
 let person: Person = {
     name: "Ice",
     age: 1,
-    isStudent: true
+    isStudent: true,
+    address: {
+        street: "123 main",
+        city: "any",
+        country: "ph"
+    }
 }
 
 let person2: Person = {
     name: "Ikeda",
     age: 4,
-    isStudent: true
+    isStudent: true,
+        address: {
+        street: "123 main",
+        city: "any",
+        country: "ph"
+    }
 }
 
