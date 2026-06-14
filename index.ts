899299a645a44e955a5f513024a0abe6eb0136b3
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-14 22:05:00 +0800

    Solve challenge

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
 type Pizza = {
+    id: number
     name: string
     price: number
 }
-
-/**
- * Challenge: using literal types and unions, update the Order status so that
- * it can only ever be "ordered" or "completed"
- */
 
 type Order = {
     id: number
@@ -16,10 +12,12 @@ type Order = {
 
 let menu: Pizza[] = [
     {
+        id: 1,
         name: "Cobra",
         price: 10
     },
     {
+        id: 2,
         name: "Sting",
         price: 15
     },
@@ -57,9 +55,9 @@ function completedOrder(orderId: number) {
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
+addNewPizza({ id: 3, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: 4, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: 5, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completedOrder(1)
@@ -67,3 +65,34 @@ completedOrder(1)
 console.log("Menu", menu)
 console.log("Cash in register:", cashInRegister)
 console.log("Order queue:", orderQueue)
+
+/**
+ * Challenge: create a new utility function called getPizzaDetail. It will take
+ * a parameter called `identifier`, but there's a twist: we want this identifier
+ * to be allowed to either be the string name of the pizza (e.g. "Pepperoni"),
+ * OR to be the number ID of the pizza (e.g. 2).
+ * 
+ * Don't worry about the code inside the function yet, just create the function
+ * signature, making sure to teach TS that the `identifier` parameter is allowed
+ * to either be a string or a number.
+ */
+
+function getPizzaDetail(identifier: string | number) {
+    let pizza;
+
+    if (typeof(identifier) === "string") {
+        pizza = menu.find(obj => obj.name === identifier)
+    } else {
+        pizza = menu.find(obj => obj.id === identifier)
+    }
+
+    if (!pizza) {
+        console.error(`Pizza ${pizza} not found.`)
+        return
+    }
+
+    return pizza
+}
+
+const pizzaDeatail = getPizzaDetail("Sting")
+console.log(pizzaDeatail)
