77936b5a2120de68c1f0792ae43365ef5b4d5643
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-07-01 21:38:59 +0800

    Complete refactor for:  Be explicit whenever you can

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-const menu = [
-    {
-        name: "Cobra",
-        price: 10
-    },
-    {
-        name: "Sting",
-        price: 15
-    },
-]
+import { getPizzaDetail } from "./index.ts";
 
-const cashInRegister = 100
-const orderQueue = []
-const nextOrderId = 1;
+const result = getPizzaDetail(false)
 
-function addNewPizza(pizzaObject) {
-    menu.push(pizzaObject)
-}
-
-
-function placeOrder(pizzaName) {
-    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
-    cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered"}
-    orderQueue.push(newOrder)
-    return newOrder
-}
-
-function completedOrder(orderId) {
-    const order = orderQueue.find(order => order.id === orderId)
-    order.status = "completed"
-    return order
-}
-
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
-
-placeOrder("Chicken Bacon Ranch")
-completedOrder("1")
-
-console.log("Menu", menu)
-console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+console.log(result)
--- index.ts
+++ index.ts
@@ -51,8 +51,10 @@ function completeOrder(orderId: number) {
 export function getPizzaDetail(identifier: string | number) {
     if (typeof identifier === "string") {
         return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
-    } else {
+    } else if (typeof identifier === "number"){
         return menu.find(pizza => pizza.id === identifier)
+    } else {
+        throw new TypeError("Parameter `identifier` must be either a string or number")
     }
 }
 
