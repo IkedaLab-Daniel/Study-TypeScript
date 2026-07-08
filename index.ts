2ad296eb75377a50aee5f099a06abe77c381e636
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-07-08 23:29:12 +0800

    Add automatic ids to menu items: Challenge 1.5 done

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ type Order = {
     status: "ordered" | "completed"
 }
 
+let cashInRegister = 100
+let nextOrderId = 1
 let nextPizzaId = 1
 
 const menu: Pizza[] = [
@@ -19,24 +21,30 @@ const menu: Pizza[] = [
     { id: nextPizzaId++, name: "Veggie", price: 9 },
 ]
 
-let cashInRegister = 100
-let nextOrderId = 1
 const orderQueue: Order[] = []
 
-function addNewPizza(pizzaObj: Pizza): void {
-    menu.push(pizzaObj)
+function addNewPizza(pizzaObj: any): void {
+    let returnObj = {
+        id: nextPizzaId++,
+        name: pizzaObj.name,
+        price: pizzaObj.price
+    }
+
+    menu.push(returnObj)
 }
 
 /**
- * Challenge part 1: Make it so we can use a global variable to track the nextPizzaId
- * and use the same trick we use with `nextOrderId++` when you're calling addNewPizza.
- * Update the menu items to use this as well so we don't have to manually enter ids 1-4
- * like we're currently doing
+ * Challenge part 1.5: Try to move the logic for adding an ID to the pizza objects 
+ * inside the addNewPizza function, so that we can call addNewPizza with no id, and
+ * the function will handle that part for us.
+ * 
+ * NOTE: you will run into TS warnings that we'll address soon, but the code should
+ * still run.
  */
 
-addNewPizza({ id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: nextPizzaId++, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: nextPizzaId++, name: "Spicy Sausage", price: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 
 
